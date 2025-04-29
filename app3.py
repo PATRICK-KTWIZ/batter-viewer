377486dca2f94dbbf5c3eece743f185ba428eb77
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2025-04-29 16:54:26 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -156,7 +156,7 @@ def show_login_page():
     <style>
         /* 제목 상단 여백 조정 */
         h1 {
-            margin-top: 20px;       /* 상단 여백 */
+            margin-top: 10px;       /* 상단 여백 */
             margin-bottom: 30px;    /* 하단 여백 */
             padding-left: 10px;     /* 왼쪽 패딩 */
             line-height: 1.3;       /* 줄 간격 */
