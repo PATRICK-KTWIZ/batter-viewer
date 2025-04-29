add316c05dfa58f444b2e425fc51fed23cf2c115
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2025-04-29 16:29:03 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -156,7 +156,26 @@ def show_login_page():
     # 로그인 페이지 클래스 추가
     st.markdown('<div class="login-page">', unsafe_allow_html=True)
 
-    st.markdown("<h1 style='text-align: left'><span style='color: #c0c0c0;'>KT WIZ</span> <span style='color: red;'>BATTING ANALYTICS</span> <span style='color: #c0c0c0;'>PAGE[Multiple Choice]</span></h1>", unsafe_allow_html=True)
+        st.markdown("""
+    <style>
+        /* 제목 상단 여백 조정 */
+        h1 {
+            margin-top: 20px;       /* 상단 여백 */
+            margin-bottom: 30px;    /* 하단 여백 */
+            padding-left: 10px;     /* 왼쪽 패딩 */
+            line-height: 1.3;       /* 줄 간격 */
+        }
+        
+        /* 제목 내 텍스트 간격 조정 */
+        h1 span {
+            margin-right: 10px;     /* 각 span 요소 사이의 간격 */
+        }
+    </style>
+        """, unsafe_allow_html=True)
+        
+
+    st.markdown("<h1 style='text-align: left'><span style='color: #c0c0c0;'>KT WIZ</span> <span style='color: red;'>BATTING ANALYTICS</span> <span style='color: #c0c0c0;'>PAGE[Multiple Choice]</span></h1>"
+                , unsafe_allow_html=True)
     # Main layout with two columns
     left_col, middle_col, right_col = st.columns([0.5, 3, 4])
 
