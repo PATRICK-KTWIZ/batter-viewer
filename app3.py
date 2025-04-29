7c679576f75470b6849f7f2acf4d7a105380c9f1
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2025-04-29 16:39:47 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -36,8 +36,6 @@ st.markdown("""
         margin: 20px auto;
         padding: 20px;
         background-color: #f0f0f0;
-        border-radius: 0px;
-        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
     }
     /* 로고 컨테이너 */
     .logo-container {
