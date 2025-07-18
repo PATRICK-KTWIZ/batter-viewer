20c2cb2b4e29c88095ef2bce8d649eedb5a05e2c
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2025-07-18 12:51:04 +0900

    Update map.py

--- map.py
+++ map.py
@@ -1271,11 +1271,11 @@ def season_hangtime_spraychart_combined(fastball_data, non_fastball_data, batter
     fig.add_shape(type="rect", x0=0, y0=0, x1=135, y1=135, line=dict(color="rgba(108,122,137,0.7)"), line_width=5)
 
     # 연장선
-    fig.add_shape(type="line", x0=120, y0=28, x1=28, y1=28,
-                  line=dict(color="rgba(108,122,137,0.9)", line_width=3, dash="dash"))
+    fig.add_shape(type="rect", x0=0, y0=28, x1=135, y1=135,
+                  line=dict(color="rgba(108,122,137,0.7)", line_width=3, dash="dash"))
     # 연장선
-    fig.add_shape(type="line", x0=28, y0=120, x1=28, y1=28,
-                  line=dict(color="rgba(108,122,137,0.9)", line_width=3, dash="dash"))
+    fig.add_shape(type="rect", x0=28, y0=0, x1=135, y1=135,
+                  line=dict(color="rgba(108,122,137,0.7)", line_width=3, dash="dash"))
     
     # 외야 경계선 (호 모양)
     fig.add_shape(type="path", path="M 0,100 Q 120,120 100,0", line_color="rgba(108,122,137,0.7)", line_width=5)
